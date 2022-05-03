1f9dd47f23a064dd03910495fa1f3eec484bdc57
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-03 12:01:08 +0200

    final

--- app.js
+++ app.js
@@ -1,69 +1,95 @@
 // Load info
 
-const playerLog = document.querySelector('h2')
-
+const playerLog = document.querySelector("h2");
 
 let gameEnabled = true;
 
 let actifPlayer = "X";
 
-let stateGame = ["", "", "", "", "", "", "", "", ""]
+let stateGame = ["", "", "", "", "", "", "", "", ""];
 
 const conditionsWin = [
-
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-
-
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 // Message in game
 
+const win = () => `The player ${actifPlayer} is the winner`;
+const egality = () => "Egality";
+const tourPlayer = () => `It's Player ${actifPlayer} turn`;
 
-const win = () => `Le joueur ${actifPlayer} à gagné`
-const equality = () => "Egalité"
-const tourPlayer = () => `C'est au tour du joueur ${actifPlayer}`
+playerLog.innerHTML = tourPlayer();
 
-playerLog.innerHTML = tourPlayer()
+document
+  .querySelectorAll(".case")
+  .forEach((cell) => cell.addEventListener("click", gestionClic));
 
-document.querySelectorAll(".case").forEach(cell => cell.addEventListener("click", gestionClic))
+document.querySelector("#restart").addEventListener("click", restart);
 
-document.querySelector("#restart").addEventListener("click", restart)
+function gestionClic() {
+  // Recupere la case cliqué (index)
 
+  const indexCase = parseInt(this.dataset.index);
 
-function gestionClic(){
+  if (stateGame[indexCase] !== "" || !gameEnabled) {
+    return;
+  }
 
-    // Recupere la case cliqué (index)
+  // On met la croix dans le jeu
 
-    const indexCase = parseInt(this.dataset.index)
+  stateGame[indexCase] = actifPlayer;
+  this.innerHTML = actifPlayer;
 
-    if (stateGame[indexCase] !== ""  || !gameEnabled){
-        return
-    }
-
-    // On met la croix dans le jeu
-
-    stateGame[indexCase] = actifPlayer
-    this.innerHTML = actifPlayer
-
-
-    checkWin()
+  checkWin();
 }
 
+// Fonction qui va vérifier la victoire/défaite/egalité
 
 function checkWin() {
+  let tourWin = false;
 
-    let tourWin = false
+  for (const conditionWin of conditionsWin) {
+    let value1 = stateGame[conditionWin[0]];
+    let value2 = stateGame[conditionWin[1]];
+    let value3 = stateGame[conditionWin[2]];
 
-    for (const conditionWin of conditionsWin) {
-        
+    if (value1 == "" || value2 == "" || value3 == "") {
+      continue;
     }
+    if (value1 == value2 && value2 == value3) {
+      tourWin = true;
+      break;
+    }
+  }
 
+  if (tourWin) {
+    playerLog.innerHTML = win();
+    gameEnabled = false;
+    return;
+  }
+
+  if (!stateGame.includes("")) {
+    playerLog.innerHTML = egality();
+    gameEnabled = false;
+    return;
+  }
+
+  actifPlayer = actifPlayer == "X" ? "O" : "X";
+  playerLog.innerHTML = tourPlayer();
+}
+
+// Fonction qui remet tout à zéro lorsqu'on appuie sur le bouton
+function restart() {
+  actifPlayer = "X" ? "O" : "X";
+  gameEnabled = true;
+  stateGame = ["", "", "", "", "", "", "", "", ""];
+  playerLog.innerHTML = tourPlayer();
+  document.querySelectorAll(".case").forEach((cell) => (cell.innerHTML = ""));
 }
